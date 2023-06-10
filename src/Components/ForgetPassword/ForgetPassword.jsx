c0e7931ca4759d1364008fd88776f74ca7787f38
Seema-Fahmawe <seemafahmawe2000@gmail.com>
2023-06-11 00:02:31 +0300

    edit password

--- src/Components/ForgetPassword/ForgetPassword.jsx
+++ src/Components/ForgetPassword/ForgetPassword.jsx
@@ -2,45 +2,61 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-
+import Joi from 'joi';
 const ForgetPassword = ({ users }) => {
 
     const [email, setEmail] = useState('');
     const navigate = useNavigate();
-
-    const [cEmail, setConfirmEmail] = useState(false);
-
+    const [cEmail, setConfirmEmail] = useState();
+    const [errors, setErrors] = useState([]);
     const onChange = (e) => {
         const { value } = e.target;
         setEmail(value);
     }
-    
+
+    const validateUser = () => {
+        const schema = Joi.object({
+            email: Joi.string()
+                .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).messages({
+                    'string.empty': 'email is required'
+                }),
+        })
+        return schema.validate({ email }, { abortEarly: false });
+    }
+
     const onSubmit = async (e) => {
         e.preventDefault();
-        
-        {
-            users.map((user) => {
-                if (user.email != `${email}`) {
-                    setConfirmEmail(false);
-                    return ;
-                } else {
-                    setConfirmEmail(true);
-                    console.log('welcome');
-                    return ;
-                }
+        const validation = validateUser();
+        const errorList = [];
+        if (validation.error) {
+            validation.error.details.map((err) => {
+                errorList.push(err.message);
+                setErrors(errorList);
             })
-        }
+        } else {
+            {
+                users.map((user) => {
+                    if (user.email == `${email}`) {
+                        setConfirmEmail(true);
+                        return;
+                    } else {
+                        setConfirmEmail(false);
+                        return;
+                    }
+                })
+            }
 
-        const result = await axios.patch('https://lazy-blue-sockeye-gear.cyclic.app/api/v1/auth/forgetPassword', { email });
-        if (email.length < 8) {
-            toast.warning('Please enter valid email');
-        }
-        else{
-            if (!cEmail) {
+            const result = await axios.patch('https://lazy-blue-sockeye-gear.cyclic.app/api/v1/auth/forgetPassword', { email });
+            if (email.length < 8) {
                 toast.warning('Please enter valid email');
-            } else {
-                toast.success('Please check your email');
-                navigate(`/resetPassword/${email}`);
+            }
+            else {
+                if (!cEmail) {
+                    toast.warning('Please enter valid email');
+                } else {
+                    toast.success('Please check your email');
+                    navigate(`/resetPassword/${email}`);
+                }
             }
         }
     }
@@ -54,6 +70,9 @@ const ForgetPassword = ({ users }) => {
             </div>
             <div className="card p-5 w-50 m-auto">
                 <form method="POST" action="/handleLogin" onSubmit={onSubmit} >
+                    {errors.map((err, index) => {
+                        return <div className='alert alert-danger' key={index}>{err}</div>
+                    })}
                     <input className="form-control" placeholder="Enter your email" type="text" name="email" onChange={onChange} />
                     <button className="btn btn-default-outline my-4 w-100 rounded">Send Code</button>
 
--- src/Components/ResetPassword/ResetPassword.jsx
+++ src/Components/ResetPassword/ResetPassword.jsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Joi from 'joi';
 import React, { useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
@@ -8,6 +9,7 @@ const ResetPassword = () => {
         code: '',
         newPassword: ''
     });
+    const [errors, setErrors] = useState([]);
     const navigate = useNavigate();
     const { email } = useParams();
 
@@ -16,15 +18,36 @@ const ResetPassword = () => {
         setInputField({ ...inputField, [name]: value });
     }
 
+    const validateUser = () => {
+        const schema = Joi.object({
+            code: Joi.string().required(),
+            newPassword: Joi.string()
+                .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required().messages({
+                    'string.empty': 'Password is required',
+                    'string.pattern.base': 'password is incorrect',
+                }),
+
+        })
+        return schema.validate(inputField, { abortEarly: false });
+    }
+
     const onSubmit = async (e) => {
         e.preventDefault();
-        const result = await axios.patch('https://lazy-blue-sockeye-gear.cyclic.app/api/v1/auth/sendCode', { ...inputField, email })
-        console.log(result.data);
-        if (result.data.message === 'success') {
-            toast.success('Changed Password Successfully!');
-            navigate('/login');
+        const validation = validateUser();
+        const errorList = [];
+        if (validation.error) {
+            validation.error.details.map((err) => {
+                errorList.push(err.message);
+                setErrors(errorList);
+            })
         } else {
-            toast.error('Please enter the right code');
+            const result = await axios.patch('https://lazy-blue-sockeye-gear.cyclic.app/api/v1/auth/sendCode', { ...inputField, email })
+            if (result.data.message === 'success') {
+                toast.success('Changed Password Successfully!');
+                navigate('/login');
+            } else {
+                toast.error('Please enter the right code');
+            }
         }
     }
 
@@ -36,7 +59,9 @@ const ResetPassword = () => {
             </div>
             <div className="card p-5 w-50 m-auto">
                 <form method="POST" action="/handleLogin" onSubmit={onSubmit}>
-
+                    {errors.map((err, index) => {
+                        return <div className='alert alert-danger py-1' key={index}>{err}</div>
+                    })}
                     <input className="form-control" placeholder=" Enter the Code" type="text" name="code" onChange={onChange} />
                     <input className="form-control my-4 " placeholder=" Enter the new Password" type="text" name="newPassword" onChange={onChange} />
                     <button className="btn btn-default-outline my-4 w-100 rounded">Reset Password</button>
